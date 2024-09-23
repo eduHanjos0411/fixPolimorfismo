0c6a110ce06923e7d24c6ef412e79b78ce240d67
Eduardo Henrique <eduardo.h.anjos0411@gmail.com>
2024-09-23 16:24:44 -0300

    Initial commit

---
+++ fixPolimorfismo/src/entities/UsedProduct.java
@@ -0,0 +1,30 @@
+package entities;
+
+import java.time.LocalDate;
+
+public class UsedProduct extends Product{
+
+	private LocalDate manufactureDate;
+	
+	public UsedProduct() {
+		
+	}
+
+	public UsedProduct(String name, Double price, LocalDate manufactureDate) {
+		super(name, price);
+		this.manufactureDate = manufactureDate;
+	}
+
+	public LocalDate getManufactureDate() {
+		return manufactureDate;
+	}
+
+	public void setManufactureDate(LocalDate manufactureDate) {
+		this.manufactureDate = manufactureDate;
+	}
+	
+	@Override
+	public String priceTag() {
+		return name + " (used) " + "$ " + price + "(Manufacture date: " + manufactureDate;
+	}
+}
